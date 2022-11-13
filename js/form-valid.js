ce9a631ed64ca62cddebfef2dde1df395f518934
JenAdamenko <adamiea8774@gmail.com>
2022-11-13 22:06:51 +0300

    добавляет работу с формой и валидацию полей формы

--- js/form-valid.js
+++ js/form-valid.js
@@ -1,22 +1,94 @@
-// const newImageForm = document.querySelector('.img-upload__form');
+//  +-  6. Напишите код для валидации формы добавления изображения, используя библиотеку Pristine (/vendor/pristine).
+// Список полей для валидации:
+// +- Хэш-теги Pristine  - выводит только одно сообщение - о длине хештега менее 2 символов
+// +  Комментарий
+// + - 7. Реализуйте логику проверки так, чтобы, как минимум, она срабатывала при попытке отправить форму и не давала этого сделать,
+//  если форма заполнена не по правилам. При желании, реализуйте проверки сразу при вводе значения в поле.
 
-// const pristin = new Pristine (newImageForm, {
-//   classTo: 'img-upload__error-hashtags',
-//   errorTextParent: 'img-upload__error-hashtags',
-//   errorTextClass: 'img-upload__error-text',
-// });
+// - Валидация хеш-тегов?
+// Для валидации хэш-тегов вам придётся вспомнить, как работать с массивами. Набор хэш-тегов можно превратить в массив,
+// воспользовавшись методом .split(). Он разбивает строки на массивы. После этого, вы можете написать цикл, который будет ходить
+// по полученному массиву и проверять каждый из хэш-тегов на предмет соответствия ограничениям.
+// Если хотя бы один из тегов не проходит нужных проверок, показывать сообщение об ошибке.
 
-// newImageForm.addEventListener ('submit', (evt) => {
-//   evt.preventDefault ();
-//   const isValid = pristin.validate ();
-//   if (isValid) {
-//     console.log('Форма заполнена верно');
-//   } else {
-//     console.log('Форма заполнена не верно');
-//   }
-// });
-// const hashtags = document.querySelector('.text__hashtags');
-// const hashtag = /^#[a-zа-яё]{1,19}\s$/i;
+// Поля, не перечисленные в техзадании, но существующие в разметке, особой валидации не требуют.
+const newImageForm = document.querySelector('.img-upload__form');//форма загрузки фото
+const hashtagsField = newImageForm.querySelector('.text__hashtags');//поле загрузки хештега
+
+const commentField = document.querySelector('.text__description');//поле ввода коммен-я
+const buttonSubmit = document.querySelector('.img-upload__submit');//кнопка отправки данных на сервер
+
+// валидация полей хэштегов и комментов
+const pristine = new Pristine(newImageForm, {
+  classTo: 'img-upload__error-hashtags',
+  errorClass: 'img-upload__error-hashtags--invalid',
+  successClass: 'img-upload__error-hashtags--valid',
+  errorTextParent: 'img-upload__error-hashtags',
+  errorTextTag: 'span',
+  errorTextClass: 'img-upload__error-text',
+});
+
+function validateHashtags (value) {
+  return value.length >= 2 && value.length <= 20;
+}
+
+pristine.addValidator(//валидация поля хештегов
+  hashtagsField,
+  validateHashtags,
+  // 'От 2 до 20 символов'
+);
+
+
+newImageForm.addEventListener ('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+  // const isValid = pristin.validate ();
+  // if (isValid) {
+  //   console.log('Форма заполнена верно');
+  // } else {
+  //   console.log('Форма заполнена не верно');
+  // }
+});
+
+function validateComments (value) {
+  return value.length < 140;
+}
+
+pristine.addValidator(//валидация поля коментов
+  commentField,
+  validateComments,
+  // 'Не более 140 символов'
+);
+
+
+newImageForm.addEventListener ('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+  // const isValid = pristin.validate ();
+  // if (isValid) {
+  //   console.log('Форма заполнена верно');
+  // } else {
+  //   console.log('Форма заполнена не верно');
+  // }
+});
+
+// блокировка кнопки отправки
+hashtagsField.addEventListener ('input', () => {
+  if (hashtagsField.value.length >= 20) {
+    buttonSubmit.disabled = true;
+    // buttonSubmit.setAttribute ('disabled',true)- или так
+  }
+});
+
+commentField.addEventListener ('input', () => {
+  if (commentField.value.length >= 140) {
+    buttonSubmit.disabled = true;
+    // buttonSubmit.setAttribute ('disabled',true)- или так
+  }
+});
+
+// if (hashtagsField.value = /^#[a-zа-яё]{1,19}\s$/i)
+
 // // console.log(hashtag.test ('#hello'));
 // console.log(hashtag.test (hashtags.value));
 // слушатель input - функция проверки хэштегов и комментов
--- js/form.js
+++ js/form.js
@@ -13,65 +13,52 @@
 // Например, всем обязательным полям нужно добавить атрибут required.
 // Затем проверьте, правильные ли типы стоят у нужных полей, если нет — проставьте правильные.
 
-// 4. Изучите, что значит загрузка изображения, и как, когда и каким образом показывается форма редактирования изображения.
+// + 4. Изучите, что значит загрузка изображения, и как, когда и каким образом показывается форма редактирования изображения.
 // Напишите код и добавьте необходимые обработчики для реализации этого пункта техзадания.
 // В работе вы можете опираться на код показа окна с полноразмерной фотографией
-// !! Важно. Подстановка выбранного изображения в форму — это отдельная домашняя работа.
-// В данном задании этот пункт реализовывать не нужно.
-
+// !! Важно. Подстановка выбранного изображения в форму — это отдельная домашняя работа.В данном задании этот пункт реализовывать не нужно.
 // У элемента .img-upload__overlay удаляется класс hidden, а body задаётся класс modal-open.
 
-// import{viewPhotoNoScroll} from './full-photo.js';
-// const uploadingImage = document.querySelector('.img-upload__overlay');//форма редактирования изображения
-// uploadingImage.classList.remove('hidden');
-// viewPhotoNoScroll.classList.add('modal-open');
+import{viewPhotoNoScroll} from './full-photo.js';
+const uploadingImage = document.querySelector('.img-upload__overlay');//форма редактирования изображения
+uploadingImage.classList.remove('hidden');
+viewPhotoNoScroll.classList.add('modal-open');
 
 // Масштаб:
-//+  При нажатии на кнопки .scale__control--smaller и .scale__control--bigger должно изменяться значение поля .scale__control--value;
-// +- При изменении значения поля .scale__control--value изображению внутри .img-upload__preview должен добавляться
+// +  При нажатии на кнопки .scale__control--smaller и .scale__control--bigger должно изменяться значение поля .scale__control--value;
+// + При изменении значения поля .scale__control--value изображению внутри .img-upload__preview должен добавляться
 // соответствующий стиль CSS, который с помощью трансформации scale задаёт масштаб.
 // Например, если в поле стоит значение 75%, то в стиле изображения должно быть написано transform: scale(0.75).
 
-// const buttonSmaller = document.querySelector('.scale__control--smaller');
-// const buttonBigger = document.querySelector('.scale__control--bigger');
-// const inputScaleValue = document.querySelector('.scale__control--value');//размер изображения
-// buttonSmaller.addEventListener ('click', () => {
-//   inputScaleValue.value = (parseInt(inputScaleValue.value, 10) - 25) + '%';
-// });
+const buttonSmaller = document.querySelector('.scale__control--smaller');// кнопка регулирования -
+const buttonBigger = document.querySelector('.scale__control--bigger');// кнопка регулирования +
+const inputScaleValue = document.querySelector('.scale__control--value');//размер изображения
 
-// buttonBigger.addEventListener ('click', () => {
-//   inputScaleValue.value = (parseInt(inputScaleValue.value, 10) + 25) + '%';
-// });
-// При изменении значения поля .scale__control--value изображению внутри .img-upload__preview должен добавляться
-// // соответствующий стиль CSS
-// const uploadPreview = document.querySelector('.img-upload__preview');
-// const uploadPreviewImg = uploadPreview.querySelector('img'); //картинка внутри .img-upload__preview
+const uploadPreviewContainer = document.querySelector('.img-upload__preview');
+const uploadPreviewImg = uploadPreviewContainer.querySelector('img'); //картинка внутри .img-upload__preview
 
-// uploadPreviewImg.style.transform.scale = (0.75); !!не понятно
-// uploadPreviewImg.style.transform = scale (0.75); !!не понятно
+buttonSmaller.addEventListener ('click', () => {
+  inputScaleValue.value = `${parseInt(inputScaleValue.value, 10) - 25}%`;
+  uploadPreviewImg.style = `transform: scale(${parseInt(inputScaleValue.value, 10) / 100})`;
+});
 
-// Эффект:
+buttonBigger.addEventListener ('click', () => {
+  inputScaleValue.value = `${parseInt(inputScaleValue.value, 10) + 25}%`;
+  uploadPreviewImg.style = `transform: scale(${parseInt(inputScaleValue.value, 10) / 100})`;
+});
+
+// + Эффект:
 // При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio, добавить картинке внутри .img-upload__preview
 // CSS-класс, соответствующий эффекту. Например, если выбран эффект .effect-chrome, изображению нужно добавить класс effects__preview--chrome.
-// .effects__preview--sepia
-// .effects__preview--marvin
-// .effects__preview--phobos
-// .effects__preview--heat
+const effectsFieldset = document.querySelector('.effects');// fieldset
 
-// const effectsList = document.querySelector('.effects__list');//список
-// const effectsListItem = effectsList.querySelectorAll('.effects__item'); //li все
+function onEffectChange (evt) {
+  if (evt.target.matches('input[type="radio"]')){
+    uploadPreviewImg.classList = `effects__preview--${evt.target.value}`;
+  }
+}
 
-// const addCheckHandler = function (item) {
-//   const radio = item.querySelector('.effects__radio');
-//   radio.addEventListener('change', () => {
-//     // console.log('получилось');
-//     uploadPreviewImg.classList.add('effects__preview--chrome');
-//   });
-// };
-
-// for (let i = 0; i < effectsListItem.length; i++) {
-//   addCheckHandler(effectsListItem[i]);
-// }
+effectsFieldset.addEventListener ('change', onEffectChange);
 
 // + 5. После реализуйте закрытие формы.
 // !! Обратите внимание
@@ -85,41 +72,26 @@
 //+  либо нажатием клавиши Esc.
 //+  Элементу .img-upload__overlay возвращается  hidden. У элемента body удаляется  modal-open.
 
-// const buttonUploadingCancel = document.querySelector('#upload-cancel');//Кнопка для закрытия формы
-// buttonUploadingCancel.addEventListener (('click'), () => {
-//   uploadingImage.classList.add('hidden');
-//   viewPhotoNoScroll.classList.remove('modal-open');
-//   const inputUploadFile = document.querySelector('#upload-file');//поле загрузки изображения
-//   inputUploadFile.value = '';// сброс поле загрузки изображения  ?Так у него вообще value нет О_О
-//   inputScaleValue.value = '100%';// сброс размер изображения
-// });
+const buttonUploadingCancel = document.querySelector('#upload-cancel');//Кнопка [Х] для закрытия формы
+buttonUploadingCancel.addEventListener (('click'), () => {
+  uploadingImage.classList.add('hidden');
+  viewPhotoNoScroll.classList.remove('modal-open');
+  const inputUploadFile = document.querySelector('#upload-file');//поле загрузки изображения
+  inputUploadFile.value = '';// сброс поле загрузки изображения  ?Так у него вообще value нет О_О
+  inputScaleValue.value = '100%';// сброс размер изображения
+});
 
-// document.addEventListener ('keydown', (evt) => {
-//   if (evt.key === 'Escape') {
-//     uploadingImage.classList.add('hidden');
-//     viewPhotoNoScroll.classList.remove('modal-open');
-//   }
-// });
-
-// файл form-valid.js +-  6. Напишите код для валидации формы добавления изображения, используя библиотеку Pristine (/vendor/pristine).
-// Список полей для валидации:
-// +- Хэш-теги
-// --  Комментарий
-
-
-// 7. Реализуйте логику проверки так, чтобы, как минимум, она срабатывала при попытке отправить форму и не давала этого сделать,
-//  если форма заполнена не по правилам. При желании, реализуйте проверки сразу при вводе значения в поле.
-
-
-// Как отменить обработчик Esc при фокусе?
+// + Как отменить обработчик Esc при фокусе?
 // Задача не имеет одного верного решения, однако намекнём на самый простой — использовать stopPropagation для события нажатия
 // клавиш в поле при фокусе.
 
-
-// Валидация хеш-тегов?
-// Для валидации хэш-тегов вам придётся вспомнить, как работать с массивами. Набор хэш-тегов можно превратить в массив,
-// воспользовавшись методом .split(). Он разбивает строки на массивы. После этого, вы можете написать цикл, который будет ходить
-// по полученному массиву и проверять каждый из хэш-тегов на предмет соответствия ограничениям.
-// Если хотя бы один из тегов не проходит нужных проверок, показывать сообщение об ошибке.
-
-// Поля, не перечисленные в техзадании, но существующие в разметке, особой валидации не требуют.
+document.addEventListener ('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    if (document.querySelector('.text__hashtags') === document.activeElement || document.querySelector('.text__description') === document.activeElement) {
+      evt.stopPropagation();
+    } else {
+      uploadingImage.classList.add('hidden');
+      viewPhotoNoScroll.classList.remove('modal-open');
+    }
+  }
+});
