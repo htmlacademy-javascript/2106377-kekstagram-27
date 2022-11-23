b45bae50df1ddd3786e959c9d0f0fc571d87ed7c
JenAdamenko <adamiea8774@gmail.com>
2022-11-23 20:15:15 +0300

    исправляет рендеринг фото

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,13 +4,6 @@ const RANDOM_PHOTO_COUNT = 10;
 const containerUsersPhoto = document.querySelector('.pictures');// контейнер для изображений др пользователей
 const templateUsersPhoto = document.querySelector('#picture').content.querySelector('.picture');// шаблон
 const filtersForm = document.querySelector('.img-filters__form');//форма с кнопкками филтров
-// const lastElement = containerUsersPhoto.querySelector('.img-upload');
-// console.log(lastElement);
-const div = document.createElement('div');
-div.classList.add('el'); // Добавляем класс 'el'
-// console.log(lastElement);
-
-// containerUsersPhoto.appendChild(div);
 
 const comparePhotos = (a, b) => b.comments.length - a.comments.length;
 
@@ -26,10 +19,9 @@ const renderThumbnails = (similarPhoto, button = 'filter-default') => {
         photoElement.addEventListener('click', (evt) => drawFullPhoto(desc, evt));
         similarPhotoFragment.append(photoElement);// добавляю клоннированный элемент во фрагмент
       });
-      containerUsersPhoto.innerHTML = '';
-      // div.append(similarPhotoFragment);// добавляю клоннированный элемент в контейнер .pictures
-      // containerUsersPhoto.append(div);// добавляю клоннированный элемент в контейнер .pictures
-      containerUsersPhoto.append(similarPhotoFragment);// добавляю клоннированный элемент в контейнер .pictures
+      // containerUsersPhoto.innerHTML = '';
+      document.querySelectorAll('.picture').forEach((elem) => elem.remove());
+      containerUsersPhoto.append(similarPhotoFragment);// добавляю клоннированный элемент в контейнер .picture
       break;
     case 'filter-random':
       similarPhoto
@@ -44,7 +36,7 @@ const renderThumbnails = (similarPhoto, button = 'filter-default') => {
           photoElement.addEventListener('click', (evt) => drawFullPhoto(desc, evt));
           similarPhotoFragment.append(photoElement);// добавляю клоннированный элемент во фрагмент
         });
-      containerUsersPhoto.innerHTML = '';
+      document.querySelectorAll('.picture').forEach((elem) => elem.remove());
       containerUsersPhoto.append(similarPhotoFragment);// добавляю клоннированный элемент в контейнер .pictures
       break;
     case 'filter-discussed':
@@ -56,36 +48,21 @@ const renderThumbnails = (similarPhoto, button = 'filter-default') => {
           photoElement.querySelector('.picture__img').src = desc.url;
           photoElement.querySelector('.picture__likes').textContent = desc.likes;
           photoElement.querySelector('.picture__comments').textContent = desc.comments.length;
-          photoElement.addEventListener('click', (evt) => drawFullPhoto(desc, evt));
           similarPhotoFragment.append(photoElement);// добавляю клоннированный элемент во фрагмент
         });
-      containerUsersPhoto.innerHTML = '';
+      document.querySelectorAll('.picture').forEach((elem) => elem.remove());
       containerUsersPhoto.append(similarPhotoFragment);
       break;
   }
 };
 
-// const clearThumbnails = () => {
-//   containerUsersPhoto.innerHTML = '';
-// };
+// const pic = document.queryeSelectorAll('.picture');
+// console.log(pic);
+// // pic.forEach((elem) => elem.remove());
 
 // блок филтров фото
 const filtersContainer = document.querySelector('.img-filters');
 filtersContainer.classList.remove('img-filters--inactive');
-
-// обработчик изменения фильтров
-
-// const setFilterClick = (cb) => {
-//   filtersForm.querySelector('#filter-default').classList.remove('img-filters__button--active');
-//   filtersForm.addEventListener (('click'), (evt) => {
-
-//     if(evt.target.classList.contains('img-filters__button')){
-//       const button = evt.target;
-//       button.classList.toggle('img-filters__button--active');
-//       cb(button.id);
-//     }
-//   });
-// };
 
 const setFilterClick = (cb) => {
   filtersForm.querySelector('#filter-default').classList.remove('img-filters__button--active');
